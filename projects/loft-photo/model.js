9fbdfbc1ec767817809a78312ebd16f77cd47cd2
pavel kuzmin <pavelwebdev26@yandex.ru>
2023-07-01 17:44:00 +0300

    ДЗ week 4

--- projects/loft-photo/model.js
+++ projects/loft-photo/model.js
@@ -1,7 +1,9 @@
 // eslint-disable-next-line no-unused-vars
-import photosDB from './photos.json';
+
 // eslint-disable-next-line no-unused-vars
-import friendsDB from './friends.json';
+
+import { resolve } from 'path';
+import { rejects } from 'assert';
 
 export default {
   getRandomElement(array) {
@@ -9,16 +11,100 @@ export default {
       return null;
     }
 
-    const index = Math.random(Math.random() * (array.length - 1));
+    // const index = Math.random(Math.random() * (array.length - 1));
 
-    return array[index]
+    // return array[index]
   },
 
-  getNextPhoto() {
-    const friend = this.getRandomElement(friendsDB);
-    const photos = photosDB.get(friend.id);
-    const photo = this.getRandomElement(photos);
+  async getNextPhoto() {
+    const friend = this.getRandomElement(this.friends.items);
+    const photos = await this.getFriendPhoto(friend.id);
+    const photo = this.getRandomElement(photos.items);
+    const size = this.findSize(photo);
 
-    return { friend, url: photo.url };
+    return { friend, id: photo.id, url: size.url };
   },
+
+  findSize(photo){
+    const size = photo.sizes.find((size) =>size.width >= 360);
+
+    if (!size){
+      return photo.size.reduce((biggest, curent) => {
+        if (curent.width > biggest.width) {
+          return curent;
+        }
+
+        return biggest;
+      }, photo.sizes[0]);
+    }
+
+    return size;
+  },
+
+  async init(){
+    this.photoCache = {};
+    this.friends = await this.getFriends();
+  },
+
+  login() {
+    return new Promise((resolve, reject) =>{
+      VK.init({
+        apiId: APP_ID,
+      });
+
+      VK.Auth.login((response) => {
+        if (response.session) {
+          resolve(response);
+        } else {
+          console.error(response);
+          reject(response);
+        }
+      }, PERM_FRIENDS | PERM_PHOTOS);
+    });
+  },
+
+  callApi(method, params) {
+    params.v = params.v || '5.120';
+
+    return new Promise((resolve, reject) => {
+      VK.api(method, params, (response) => {
+        if (response.error) {
+          reject(new Error(response.error.error_msg));
+        } else {
+          resolve(response.response);
+        }
+      });
+    });
+  },
+
+  getFriends() {
+    const params = {
+      fields: ['photo_50', 'photo_100'],
+    };
+
+    return this.callApi('friends.get', params);
+  },
+
+  getPhotos(owner) {
+    const params = {
+      owner_id: owner,
+    }
+
+    return this.callApi('photos.getAll', params);
+  },
+
+  async getFriendPhotos(id){
+    let photos = this.photoCache[id];
+
+    if (photos){
+      return photos;
+    }
+
+    photos = await this.getFriendPhotos(id);
+
+    this.photoCache[id] = photos;
+
+    return photos;
+  },
+
 };
